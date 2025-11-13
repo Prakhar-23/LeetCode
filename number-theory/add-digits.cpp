ed5672d0c2ea3694ac42f87caa6a4068c1ce5cfe
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-13 23:50:08 +0530

    LC-add-digits [Runtime: 0 ms; Memory: 8.8 MB]

--- number-theory/add-digits.cpp
+++ number-theory/add-digits.cpp
@@ -1,20 +1,16 @@
-class Solution {
+class Solution 
+{
 public:
-    int addDigits(int num) 
-    {
-        int final=0;
-        int newFinal=0;
-        if(num == 0) return 0;
-        while(num)
-        {
-            final += num%10;
-            num = num/10;
+    int addDigits(int num) {
+        while (num > 9) {         
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
         }
-        if(final > 9)
-        {
-            newFinal += final%10;
-            final = final/10;
-        }
-         return newFinal+final;
+        return num;
+    
     }
 };
