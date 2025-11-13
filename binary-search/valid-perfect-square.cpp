036fe6609b4e61b4f4e346cf691e49366296861e
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-14 00:36:49 +0530

    LC-valid-perfect-square [Runtime: N/A; Memory: N/A]

--- binary-search/valid-perfect-square.cpp
+++ binary-search/valid-perfect-square.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool isPerfectSquare(int num) {
-      int n = num/2;
+    bool isPerfectSquare(long num) {
+      long n = num/2;
       for(int i=0 ; i<=num ; i++)
       {
         if(i*i == num) return true;
