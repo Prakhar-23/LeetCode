b14a28aa5d8a64f02b0a11d69a2917a320f2d36e
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-13 23:47:44 +0530

    LC-add-digits [Runtime: N/A; Memory: N/A]

--- math/add-digits.cpp
+++ math/add-digits.cpp
@@ -3,6 +3,7 @@ public:
     int addDigits(int num) 
     {
         int final=0;
+        int newFinal=0;
         if(num == 0) return 0;
         while(num)
         {
@@ -11,12 +12,9 @@ public:
         }
         if(final > 9)
         {
-            while(num)
-         {
-            final += num%10;
-            num = num/10;
-         }
-         return final;
+            newFinal += final%10;
+            final = final/10;
         }
+         return newFinal+final;
     }
 };
