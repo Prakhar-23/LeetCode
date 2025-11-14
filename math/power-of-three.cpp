4339682079a6967b85207f2c8b030b909219c2a0
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-15 00:59:49 +0530

    LC-power-of-three [Runtime: 4 ms; Memory: 8.8 MB]

--- math/power-of-three.cpp
+++ math/power-of-three.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool isPowerOfThree(int n)
     {
-        while(n!=1 && n>0 && n%3==0)
+        if(n <= 0) return false;
+        while(n%3==0)
         {
            n /= 3;
         }
