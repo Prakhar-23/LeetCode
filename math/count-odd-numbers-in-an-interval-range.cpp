fdceec9c9a87a97e9b590f377af19e3f720250bf
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-16 22:39:59 +0530

    LC-count-odd-numbers-in-an-interval-range [Runtime: 1691 ms; Memory: 7.9 MB]

--- math/count-odd-numbers-in-an-interval-range.cpp
+++ math/count-odd-numbers-in-an-interval-range.cpp
@@ -3,10 +3,9 @@ public:
     int countOdds(int low, int high) 
     {
         int odd=0;
-        int even = 0;
         for(int i=low ; i<=high ; i++)
         {
-            if(i % 2 == 0) even ++;
+            if(i % 2 == 0) {}
             else odd++;
         }
         return odd;
