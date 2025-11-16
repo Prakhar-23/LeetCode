d60171e33af2b9cbb6ee2ec5f0b344521b68664a
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-16 21:21:10 +0530

    LC-average-salary-excluding-the-minimum-and-maximum-salary [Runtime: 0 ms; Memory: 9.7 MB]

---
+++ array/average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(),salary.end());
+        int min = salary.at(0);
+        int max = salary.back();
+        long double total = 0;
+        for(int x : salary)
+        {
+            total += x;
+        }
+        total = total - (min + max);
+        double avg = total/(salary.size()-2);
+        return avg;
+    }
+};
