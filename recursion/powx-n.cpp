21ec78d04abad079d56c56d431e4dc2edc26bdc8
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-17 23:30:36 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- recursion/powx-n.cpp
+++ recursion/powx-n.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    double myPow(double x, int n)
-    {
-        long double res=1.00;
-        if(n==0) return 1;
-        
-        else if(n<0)
-        {
-            n=-n;
-            for(double i=1;i<=n;i++)
-         {
-            res *= x;
-         }
-         return (1.00/res);
-        }
+    double myPow(double x, int n) {
+        long double res = 1.0;
 
+        if (n == 0) return 1.0;
+
+        long long N = n;   
+
+        if (N < 0)
+        {
+            N = -N;       
+            for (long long i = 1; i <= N; i++)
+            {
+                res *= x;
+            }
+            return 1.0 / res;  
+        }
         else
         {
-         for(int i=1;i<=n;i++)
-         {
-            res *= x;
-         }
-         return res;
+            for (long long i = 1; i <= N; i++)
+            {
+                res *= x;
+            }
+            return res;
         }
     }
 };
