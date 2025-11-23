f9f3ca795b2519a0731f216b7f3dc68672f8ebac
Prakhar Praveen <prakharpraveen1023@gmail.com>
2025-11-23 21:19:09 +0530

    LC-calculate-money-in-leetcode-bank [Runtime: 0 ms; Memory: 7.7 MB]

---
+++ math/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        long long w=n/7;
+        long long r=n%7;
+        long long fullweeks=28*w+7*(w*(w-1)/2);
+        long long rem = r*w+r*(r+1)/2;
+        return fullweeks + rem;
+    }
+};
