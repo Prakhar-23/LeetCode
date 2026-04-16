f6fcff0529055848d2cc2023090fec35ac5aa9f8
Prakhar Praveen <prakharpraveen1023@gmail.com>
2026-04-17 01:33:57 +0530

    LC-move-zeroes [Runtime: 1 ms; Memory: 24 MB]

--- array/move-zeroes.cpp
+++ array/move-zeroes.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0; 
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
+        int n = nums.size();
+        int i=0;
+        for(int j=0;j<n;j++)
+        {
+            if(nums[j]!=0) 
+            {
+                swap(nums[i],nums[j]);
+                i++;
+            
             }
-        }
-
-        while (j < nums.size()) {
-            nums[j] = 0;
-            j++;
         }
     }
 };
- 
