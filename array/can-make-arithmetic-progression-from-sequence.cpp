724c5a587174ecc143db09e3caddcc246b60b4e9
Prakhar Praveen <prakharpraveen1023@gmail.com>
2026-04-17 15:56:29 +0530

    LC-can-make-arithmetic-progression-from-sequence [Runtime: 0 ms; Memory: 12.8 MB]

--- array/can-make-arithmetic-progression-from-sequence.cpp
+++ array/can-make-arithmetic-progression-from-sequence.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) 
     {
+        sort(arr.begin(),arr.end());
         int n = arr.size();
-        sort(arr.begin(), arr.end());
+        int diff = arr[1]-arr[0];
 
-        int diff = arr[1] - arr[0];
-
-        for(int i = 1; i < n - 1; i++)
+        for(int i=0;i<n-1;i++)
         {
-            if(arr[i + 1] - arr[i] != diff)
+            if(arr[i+1]-arr[i] != diff)
+            {
                 return false;
+            }
         }
-
         return true;
     }
 };
