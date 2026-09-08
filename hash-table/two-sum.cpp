c1ff3598a470aba3e1b0bbeadb0da53434fdb901
Prakhar Praveen <prakharpraveen1023@gmail.com>
2026-09-09 00:37:38 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -1,22 +1,22 @@
-class Solution 
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        map<int, int> mpp;
         int n = nums.size();
-         for(int i=0;i<n;i++)
-         {
-            for(int j=i+1;j<n;j++)
-            {
-                if(i==j) continue;
-                if(nums[i]+nums[j]==target)
-                {
-                    return {i,j};
-                }
+
+        for(int i = 0; i < n; i++) {
+
+            int num = nums[i];
+            int moreNeeded = target - num;
+
+            if(mpp.find(moreNeeded) != mpp.end()) {
+                return {mpp[moreNeeded], i};
             }
-         }
-         return{};
+
+            mpp[num] = i;
+        }
+
+        return {-1, -1};
     }
 };
-
-
