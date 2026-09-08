407ba4e25ef6c187bcbce804db898878e24b01b3
Prakhar Praveen <prakharpraveen1023@gmail.com>
2026-09-09 00:18:26 +0530

    LC-two-sum [Runtime: 70 ms; Memory: 14.1 MB]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -6,7 +6,7 @@ public:
         int n = nums.size();
          for(int i=0;i<n;i++)
          {
-            for(int j=0;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
                 if(i==j) continue;
                 if(nums[i]+nums[j]==target)
